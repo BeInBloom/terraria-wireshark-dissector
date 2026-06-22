3e71476eaa193e89ba4b67929faa8750023b00ac
Sergei Skoropupov <donckou@gmail.com>
2026-06-22 19:31:29 +0300

    feat: важный коммит перед жратбл

--- terraria/packet/payload_reader.lua
+++ terraria/packet/payload_reader.lua
@@ -1,5 +1,14 @@
 local M = {}
 
+---@class TerrariaVector2
+---@field x number
+---@field y number
+
+---@class TerrariaColor
+---@field r integer
+---@field g integer
+---@field b integer
+
 ---@class PayloadReader
 ---@field ctx PacketContext
 ---@field cursor integer
@@ -29,16 +38,22 @@ function PayloadReader:range(len)
 	return range
 end
 
+---@return integer
+---@return TvbRange
 function PayloadReader:uint8()
 	local range = self:range(1)
 	return range:uint(), range
 end
 
+---@return integer
+---@return TvbRange
 function PayloadReader:uint16_le()
 	local range = self:range(2)
 	return range:le_uint(), range
 end
 
+---@return integer
+---@return TvbRange
 function PayloadReader:int16_le()
 	local range = self:range(2)
 	return range:le_int(), range
@@ -49,16 +64,18 @@ function PayloadReader:read_7bit_encoded_int()
 	local res = 0
 	local shift = 0
 
-	while true do
+	for _ = 1, 5 do
 		local byte = self:uint8()
 
 		res = res | ((byte & 0x7F) << shift)
-		if (byte & 0x80) then
+		if (byte & 0x80) == 0 then
 			return res
 		end
 
 		shift = shift + 7
 	end
+
+	error("invalid 7-bit encoded int")
 end
 
 ---@return string
@@ -67,6 +84,96 @@ function PayloadReader:string()
 	local len = self:read_7bit_encoded_int()
 	local range = self:range(len)
 	return range:string(encoding.ENC_UTF_8), range
+end
+
+---@return boolean
+---@return TvbRange
+function PayloadReader:bool()
+	local value, range = self:uint8()
+	return value ~= 0, range
+end
+
+---@return integer
+---@return TvbRange
+function PayloadReader:int32_le()
+	local range = self:range(4)
+	return range:le_int(), range
+end
+
+---@return integer
+---@return TvbRange
+function PayloadReader:uint32_le()
+	local range = self:range(4)
+	return range:le_uint(), range
+end
+
+---@return UInt64
+---@return TvbRange
+function PayloadReader:uint64_le()
+	local range = self:range(8)
+	return range:le_uint64(), range
+end
+
+---@return integer
+---@return TvbRange
+function PayloadReader:sbyte()
+	local range = self:range(1)
+	return range:int(), range
+end
+
+---@return number
+---@return TvbRange
+function PayloadReader:single_le()
+	local range = self:range(4)
+	return range:le_float(), range
+end
+
+---@param len integer
+---@return string
+---@return TvbRange
+function PayloadReader:bytes(len)
+	local range = self:range(len)
+	return range:raw(), range
+end
+
+---@param start integer
+---@return TvbRange
+function PayloadReader:range_from_start(start)
+	return self.ctx:payload_subrange(start, self.cursor - start)
+end
+
+---@return TerrariaVector2
+---@return TvbRange
+function PayloadReader:vector2()
+	local start = self.cursor
+
+	local x = self:single_le()
+	local y = self:single_le()
+
+	local range = self:range_from_start(start)
+
+	return {
+		x = x,
+		y = y,
+	}, range
+end
+
+---@return TerrariaColor
+---@return TvbRange
+function PayloadReader:color()
+	local start = self.cursor
+
+	local r = self:uint8()
+	local g = self:uint8()
+	local b = self:uint8()
+
+	local range = self:range_from_start(start)
+
+	return {
+		r = r,
+		g = g,
+		b = b,
+	}, range
 end
 
 return M
